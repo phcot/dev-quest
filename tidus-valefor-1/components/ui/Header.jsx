d4a5a011551f3b0f2c2a7bb5de305ea07563d0b5
phcot <philippe.cote.28@live.com>
2024-08-14 02:29:23 -0400

    First portfolio draft

---
+++ tidus-valefor-1/components/ui/Header.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { Button } from "./button";
+
+// Components
+import Nav from "./Nav";
+
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* logo */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Philippe<span className="text-accent">.</span>
+                    </h1>
+                </Link>
+
+                
+                {/*desktop nav and hire me button */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile nav */}
+                <div className="xl:hidden">mobile nav</div>
+
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
